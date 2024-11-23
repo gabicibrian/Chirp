12eef12d9769037b9778667f72fb128fa09a8d5b
ebreve12 <eu.breve1@gmail.com>
2024-11-23 02:46:21 -0600

    Test Login 1

--- chirp/src/screens/Login.js
+++ chirp/src/screens/Login.js
@@ -1,16 +1,37 @@
+import '../css/index.css';
 import '../css/Login.css';
+import '../css/Buttons.css';
+import '../css/Input.css';
 import Logo from '../assets/logo.svg';
-import Input from '../components/Input';
 
 const Login = () =>{
     return (
         <div className='login-container'>
-            <img src={Logo}></img>
-            <md-outlined-text-field label="Nombre de usuario" value="" className='login-input'>
-            </md-outlined-text-field>
-            <md-outlined-text-field label="Contraseña" value="" className='login-input' type="password">
-            </md-outlined-text-field>
-            <md-filled-button>Iniciar sesión</md-filled-button>
+            <div class="logo-container">
+                <img src={Logo} alt="Chirp logo" id="login-logo"></img>
+            </div>
+            
+            <div class="groups-container">
+                <md-outlined-text-field label="Nombre de usuario" value="" class='input-default label px10'>
+                </md-outlined-text-field>
+                <md-outlined-text-field label="Contraseña" type="password" class="input-default label">
+                <md-icon-button toggle slot="trailing-icon">
+                    <md-icon>visibility</md-icon>
+                    <md-icon slot="selected">visibility_off</md-icon>
+                </md-icon-button>
+                </md-outlined-text-field>
+            </div>
+          
+            <div class="groups-container-2 px130">
+                <md-filled-button class="btn-container btn-LG label px10" id="button">Iniciar sesión</md-filled-button>
+                <md-text-button class="link-sm link">¿Olvidaste tu contraseña?</md-text-button>
+            </div>
+
+            <div class="groups-container-2">
+                <p class="label-sm login-label px10">¿Aún no tienes una cuenta?</p>
+                <md-text-button class="btn-container btn-LG label">Registrarse</md-text-button>
+            </div>
+            
         </div>
     );
 }
