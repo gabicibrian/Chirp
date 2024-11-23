cbaabad660b5abedfdfd025f3d6cff111a19a2b7
ebreve12 <eu.breve1@gmail.com>
2024-11-22 21:42:33 -0600

    Test Login

--- chirp/src/index.js
+++ chirp/src/index.js
@@ -5,6 +5,7 @@ import '@material/web/icon/icon.js';
 import '@material/web/ripple/ripple.js';
 import '@material/web/button/text-button.js';
 import "@material/web/textfield/outlined-text-field.js";
+import '@material/web/button/filled-button.js';
 import './css/index.css';
 import App from './App';
 
--- chirp/src/screens/Login.js
+++ chirp/src/screens/Login.js
@@ -1,3 +1,18 @@
 import '../css/Login.css';
+import Logo from '../assets/logo.svg';
 import Input from '../components/Input';
 
+const Login = () =>{
+    return (
+        <div className='login-container'>
+            <img src={Logo}></img>
+            <md-outlined-text-field label="Nombre de usuario" value="" className='login-input'>
+            </md-outlined-text-field>
+            <md-outlined-text-field label="Contraseña" value="" className='login-input' type="password">
+            </md-outlined-text-field>
+            <md-filled-button>Iniciar sesión</md-filled-button>
+        </div>
+    );
+}
+
+export default Login
